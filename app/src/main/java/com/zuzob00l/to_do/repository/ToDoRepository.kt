692985d17092f52d1bbe9f5e3ca3621819f907d5
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-10-09 22:09:42 +0200

    collect list of Tasks in ViewModelScope

--- app/src/main/java/com/zuzob00l/to_do/repository/ToDoRepository.kt
+++ app/src/main/java/com/zuzob00l/to_do/repository/ToDoRepository.kt
@@ -2,9 +2,11 @@ package com.zuzob00l.to_do.repository
 
 import com.zuzob00l.to_do.data.ToDoDao
 import com.zuzob00l.to_do.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao)
 {
     val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
--- app/src/main/java/com/zuzob00l/to_do/viewModels/SharedViewModel.kt
+++ app/src/main/java/com/zuzob00l/to_do/viewModels/SharedViewModel.kt
@@ -1,4 +1,30 @@
 package com.zuzob00l.to_do.viewModels
 
-class SharedViewModel {
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zuzob00l.to_do.data.models.ToDoTask
+import com.zuzob00l.to_do.repository.ToDoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedViewModel @Inject constructor(
+    private val repository: ToDoRepository
+): ViewModel()
+{
+
+    private val _allTasks = MutableStateFlow<List<ToDoTask>>(emptyList())
+    val allTasks: StateFlow<List<ToDoTask>> = _allTasks
+    fun getAllTask() {
+        viewModelScope.launch {
+            repository.getAllTasks.collect {
+                //collect value:
+                _allTasks.value = it
+            }
+        }
+    }
 }
