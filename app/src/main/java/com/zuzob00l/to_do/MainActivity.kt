f9e96b0a19ebf5715c894f93407f9741832e68b1
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-10-10 20:44:36 +0200

    set the navController and call  navigation function

--- app/src/main/java/com/zuzob00l/to_do/MainActivity.kt
+++ app/src/main/java/com/zuzob00l/to_do/MainActivity.kt
@@ -10,39 +10,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.zuzob00l.to_do.navigation.SetupNavigation
 import com.zuzob00l.to_do.ui.theme.ToDoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+             navController = rememberNavController()
+             SetupNavigation(navController = navController)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDoTheme {
-        Greeting("Android")
-    }
-}
