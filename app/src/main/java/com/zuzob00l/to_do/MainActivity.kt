77f818d96d6e54618a1e7fb633768513273a5509
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-10-17 13:47:49 +0200

    create sharedViewModel object

--- app/src/main/java/com/zuzob00l/to_do/MainActivity.kt
+++ app/src/main/java/com/zuzob00l/to_do/MainActivity.kt
@@ -3,23 +3,28 @@ package com.zuzob00l.to_do
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.zuzob00l.to_do.navigation.SetupNavigation
 import com.zuzob00l.to_do.ui.theme.ToDoTheme
+import com.zuzob00l.to_do.viewModels.SharedViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
+@ExperimentalMaterialApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val sharedViewModel: SharedViewModel by viewModels()
     private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +32,9 @@ class MainActivity : ComponentActivity() {
             ToDoTheme {
 
              navController = rememberNavController()
-             SetupNavigation(navController = navController)
+             SetupNavigation(
+                 navController = navController,
+                 sharedViewModel = sharedViewModel)
             }
         }
     }
