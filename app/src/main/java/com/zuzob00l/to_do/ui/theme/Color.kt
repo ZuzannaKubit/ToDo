8e2ca47c9570c90e625539013bac9c856f66d807
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-10-17 13:46:53 +0200

    add custom colors for text and note card

--- app/src/main/java/com/zuzob00l/to_do/ui/theme/Color.kt
+++ app/src/main/java/com/zuzob00l/to_do/ui/theme/Color.kt
@@ -1,8 +1,10 @@
 package com.zuzob00l.to_do.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
@@ -15,4 +17,27 @@ val HighPriorityColor = Color(0xFFEB213F)
 val MediumPriorityColor = Color(0xFFD69A59)
 val LowPriorityColor = Color(0xFF7AA971)
 
+//Dark Theme:
+val Purple80 = Color(0xFFD0BCFF)
+val LightGray = Color(0xFF8993AB)
 val BackgroundColor = Color(0xFF454894)
+
+val Colors.topAppBarColor: Color
+    @Composable
+    get() = if(isLight) Color.White else LightGray
+
+val Colors.topAppBarBackground: Color
+    @Composable
+    get() = if(isLight) BackgroundColor else Color.Black
+
+val Colors.fabBackgroundColor: Color
+    @Composable
+    get() = if(isLight) BackgroundColor else Purple80
+
+val Colors.ListItemColor: Color
+  @Composable
+  get() = if(isLight) Purple80 else BackgroundColor
+
+val Colors.ListItemTextColor: Color
+   @Composable
+   get() = if(isLight) Color.Black else Color.White
