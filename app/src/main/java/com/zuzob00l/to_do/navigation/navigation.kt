d08472e7c413e003d051da3615762d219a8e9d29
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-10-17 13:52:07 +0200

    add navController parameter to nav destinations

--- app/src/main/java/com/zuzob00l/to_do/navigation/navigation.kt
+++ app/src/main/java/com/zuzob00l/to_do/navigation/navigation.kt
@@ -1,5 +1,6 @@
 package com.zuzob00l.to_do.navigation
 
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
@@ -7,9 +8,12 @@ import androidx.navigation.compose.NavHost
 import com.zuzob00l.to_do.navigation.destinations.listComposable
 import com.zuzob00l.to_do.navigation.destinations.taskComposable
 import com.zuzob00l.to_do.util.Constants.LIST_SCREEN
-
+import com.zuzob00l.to_do.viewModels.SharedViewModel
+@ExperimentalMaterialApi
 @Composable
-fun SetupNavigation(navController: NavHostController)
+fun SetupNavigation(
+    navController: NavHostController,
+    sharedViewModel: SharedViewModel)
 {
     val screens = remember(navController) { Screens(navController) }
 
@@ -17,7 +21,11 @@ fun SetupNavigation(navController: NavHostController)
         navController = navController,
         startDestination = LIST_SCREEN)
     {
-       listComposable(navigateToTaskScreen = screens.task)
-        taskComposable(navigateToListScreen = screens.list)
+       listComposable(
+           navigateToTaskScreen = screens.task,
+           sharedViewModel = sharedViewModel)
+
+        taskComposable(
+            navigateToListScreen = screens.list)
     }
 }
