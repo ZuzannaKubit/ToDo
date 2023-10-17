f88a75db486292aa0dcc404905ef0711d6c5712a
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-10-17 13:55:41 +0200

    add states for searchAppBar and searchTextState to SharedViewModel

--- app/src/main/java/com/zuzob00l/to_do/viewModels/SharedViewModel.kt
+++ app/src/main/java/com/zuzob00l/to_do/viewModels/SharedViewModel.kt
@@ -1,10 +1,12 @@
 package com.zuzob00l.to_do.viewModels
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zuzob00l.to_do.data.models.ToDoTask
 import com.zuzob00l.to_do.repository.ToDoRepository
+import com.zuzob00l.to_do.util.SearchAppBarState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,9 +18,14 @@ class SharedViewModel @Inject constructor(
     private val repository: ToDoRepository
 ): ViewModel()
 {
+     val searchAppBarState: MutableState<SearchAppBarState>
+    = mutableStateOf(SearchAppBarState.CLOSED)
+
+     val searchTextState: MutableState<String> = mutableStateOf("")
 
     private val _allTasks = MutableStateFlow<List<ToDoTask>>(emptyList())
     val allTasks: StateFlow<List<ToDoTask>> = _allTasks
+
     fun getAllTask() {
         viewModelScope.launch {
             repository.getAllTasks.collect {
