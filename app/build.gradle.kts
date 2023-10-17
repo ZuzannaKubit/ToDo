39bf59ecd916f92bdf474c883122a1cc433308a7
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-10-17 13:59:45 +0200

    change jvm version in gradle from 18  to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.zuzob00l.to_do"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.zuzob00l.to_do"
@@ -33,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -54,6 +54,7 @@ android {
 
 dependencies {
 
+    val room_version = "2.5.2"
     val nav_version = "2.6.0"
     //navigation
     implementation("androidx.navigation:navigation-compose:$nav_version")
@@ -65,14 +66,20 @@ dependencies {
     //database:
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    //ksp:
+    //Room:
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
     //ksp("androidx.room:room-compiler:$room_version")
     //Hilt:
     implementation ("androidx.hilt:hilt-navigation-fragment:1.0.0")
     implementation ("androidx.hilt:hilt-work:1.0.0")
     implementation ("com.google.dagger:hilt-android:2.44")
-    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+    //material:
+    implementation("androidx.compose.material:material-android:1.5.3")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
