14f3da238f09fa3fb743018f550d40bfc74102ab
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-10-17 13:58:23 +0200

    Update ListComposable.kt

--- app/src/main/java/com/zuzob00l/to_do/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/zuzob00l/to_do/navigation/destinations/ListComposable.kt
@@ -1,18 +1,25 @@
 package com.zuzob00l.to_do.navigation.destinations
 
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.zuzob00l.to_do.screens.list.ListScreen
 import com.zuzob00l.to_do.util.Constants.LIST_ARGUMENT_KEY
 import com.zuzob00l.to_do.util.Constants.LIST_SCREEN
-
-fun NavGraphBuilder.listComposable(navigateToTaskScreen:(Int) -> Unit)
+import com.zuzob00l.to_do.viewModels.SharedViewModel
+@ExperimentalMaterialApi
+fun NavGraphBuilder.listComposable(
+    navigateToTaskScreen:(Int) -> Unit,
+    sharedViewModel: SharedViewModel)
 {
     composable(
         route = LIST_SCREEN,
         arguments = listOf(navArgument(LIST_ARGUMENT_KEY) { type = NavType.StringType })
     ) {
-
+          ListScreen(
+              navigateToTaskScreen = navigateToTaskScreen,
+              sharedViewModel = sharedViewModel)
     }
 }
